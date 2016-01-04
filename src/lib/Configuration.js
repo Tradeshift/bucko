43870dac1bbc0531b3978eb1bc107a5f470a7e22
Thomas Stapleton <tst@tradeshift.com>
2016-01-04 03:27:57 -0800

    get a basic app with oauth functional

---
+++ src/lib/Configuration.js
@@ -0,0 +1,73 @@
+var _ = require('LoDash');
+var fs = require('fs');
+var path = require('path');
+
+function Configuration(data) {
+	this._data = data;
+}
+
+Configuration.prototype = {
+	getTradeshiftHost: function() {
+		return this._data.host.tradeshift;
+	},
+	getTradeshiftAuthLoginPath: function() {
+		return this._data.path.tradeshift.auth.login;
+	},
+	getTradeshiftAuthTokenPath: function() {
+		return this._data.path.tradeshift.auth.token;
+	},
+	getTradeshiftAuthLoginUri: function() {
+		return ['https://', this.getTradeshiftHost(), this.getTradeshiftAuthLoginPath()].join('');
+	},
+	getTradeshiftAuthTokenUri: function() {
+		return ['https://', this.getTradeshiftHost(), this.getTradeshiftAuthTokenPath()].join('');
+	},
+	getUIComponentsVersion: function() {
+		return this._data.uiComponents.version;
+	},
+
+	getAppId: function() {
+		return this._data.manifest.app_id;
+	},
+	getVendorId: function() {
+		return this._data.manifest.vendor_id;
+	},
+	getVersion: function() {
+		return this._data.manifest.version;
+	},
+	getClientId: function() {
+		return [this.getVendorId(), this.getAppId()].join('.');
+	},
+	getClientIdWithVersion: function() {
+		return [this.getClientId(), this.getVersion()].join('.');
+	},
+	getManifest: function() {
+		return this._data.manifest;
+	},
+
+	getAuthSecret: function() {
+		return this._data.oauth.secret;
+	},
+	getAppHost: function() {
+		return this._data.host.app;
+	},
+	getAppPaths: function() {
+		return this._data.path.app;
+	},
+	getAuthCallbackUri: function() {
+		return ['https://', this.getAppHost(), this.getAppPaths().authCallback].join('');
+	},
+};
+
+Configuration.load = function(rootPath) {
+	var config = {};
+	var files = ['defaults', 'local', 'oauth'];
+	_.forEach(files, function(value, key) {
+		var file = path.resolve(rootPath, [value, '.json'].join(''));
+		var data = JSON.parse(fs.readFileSync(file));
+		_.merge(config, data);
+	});
+	return new Configuration(config);
+};
+
+module.exports = Configuration;
---
+++ src/lib/Manifest.js
@@ -0,0 +1,52 @@
+var _ = require('LoDash');
+var fs = require('fs-extra');
+var mime = require('mime');
+var path = require('path');
+
+function buildUrl(host, path) {
+	return ['https://', host, path].join('');
+}
+
+function encodeImages(images) {
+	var output = {};
+	_.forEach(images, function(image) {
+		var file = path.resolve(__dirname, '../../images/', image);
+		var data = fs.readFileSync(file).toString('base64');
+		output[path.basename(image)] = ['data:', mime.lookup(image), ';base64,', data].join('');
+	});
+	return output;
+}
+
+function buildManifest(config) {
+	var data = _.defaults({}, config.getManifest());
+	var host = config.getAppHost();
+	var paths = config.getAppPaths();
+
+	data.app = {
+		main: buildUrl(host, paths.main),
+		redirect_uri: buildUrl(host, paths.authCallback),
+	};
+	data.capabilities = {
+		access: _.pairs(data.permissions).filter(_.last).map(_.first),
+	};
+	data.images = encodeImages(data.icons);
+	data.locales = {};
+	data.locales[data.default_locale] = {
+		app_desc: data.app_desc,
+		app_name: data.app_name,
+	};
+
+	delete data.app_desc;
+	delete data.app_name;
+	delete data.permissions;
+
+	var fileName = ['manifest-', data.app_id, '-', data.version, '.json'].join('');
+	var manifest = path.resolve(__dirname, '../../manifest/', fileName);
+	fs.outputJsonSync(manifest, data);
+
+	return fileName;
+}
+
+module.exports = {
+	build: buildManifest,
+};
